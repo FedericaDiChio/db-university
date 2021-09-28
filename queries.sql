a619ffdb70f9fd56c7502252f553bdccb1873c53
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-28 16:33:13 +0200

    es 6

--- queries.sql
+++ queries.sql
@@ -21,3 +21,16 @@ ORDER BY (`date_of_birth`) ASC;
 SELECT `name`, `period`, `year`
 FROM `courses` 
 WHERE `period` = "I semestre" AND `year` = "1";
+
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+SELECT * 
+FROM `exams`
+WHERE HOUR >= "14:00:00" AND `date` = "2020/06/20"
+ORDER BY (`hour`) ASC;
+
+
+-- 6. Selezionare tutti i corsi di laurea magistrale
+SELECT `name`, `level` 
+FROM `degrees`
+WHERE `level` = "magistrale";
