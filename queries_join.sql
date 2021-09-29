196f51bb1cff62497e99095f5aec4d0fe195035e
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-29 18:03:11 +0200

    es 3.

--- queries_join.sql
+++ queries_join.sql
@@ -21,7 +21,7 @@ JOIN `teachers`
 ON `teachers`.`id` = `course_teacher`.`teacher_id`
 JOIN `courses`
 ON `courses`.`id` = `course_teacher`.`course_id`
-WHERE `teachers`.`id` = 44;
+WHERE `teachers`.`name` = "Fulvio" AND `teachers`.`surname` = "Amato";
 
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
@@ -49,10 +49,14 @@ ON `course_teacher`.`teacher_id` = `teachers`.`id`
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 SELECT DISTINCT `teachers`.`name`, `teachers`.`surname`, `departments`.`name`
 FROM `teachers`
-JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id`
-JOIN `courses` ON `course_teacher`.`course_id` = `courses`.`id`
-JOIN `degrees` ON `courses`.`degree_id` = `degrees`.`id`
-JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+JOIN `course_teacher` 
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses` 
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees`
+ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments` 
+ON `degrees`.`department_id` = `departments`.`id`
 WHERE `departments`.`name` = 'Dipartimento di Matematica'
 ORDER BY (`teachers`.`name`) ASC, (`teachers`.`surname`) ASC;
 
