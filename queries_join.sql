e7d92e0e62ee25a3e9419126776ba1df8469d0ee
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-29 15:36:50 +0200

    es 4

--- queries_join.sql
+++ queries_join.sql
@@ -22,3 +22,38 @@ ON `teachers`.`id` = `course_teacher`.`teacher_id`
 JOIN `courses`
 ON `courses`.`id` = `course_teacher`.`course_id`
 WHERE `teachers`.`id` = 44;
+
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
+-- relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `students`.`name`,`students`.`surname`, `degrees`.`name`,`departments`.`name` 
+FROM `students`
+JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY (`students`.`name`) ASC, (`students`.`surname`) ASC;
+
+
+
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+-- NO 
+-- SELECT `teachers`.`name`, `teachers`.`surname`, `departments`.`name`
+-- FROM `teachers`
+-- JOIN `course_teacher`
+-- ON `teachers`.`id` = `course_teacher`.`teacher_id`
+-- JOIN `courses`
+-- ON `course_teacher`.`course_id`=`courses`.`id`
+-- JOIN `degrees`
+-- ON `courses`.`degree_id` = `degrees`.`id`
+-- JOIN `departments`
+-- ON `degrees`.`department_id` = `departments`.`id`
+-- WHERE `departments`.`name` = "Dipartimento di Matematica"
+-- ORDER BY(`teachers`.`surname`) ASC
+
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
+-- superare ciascuno dei suoi esami
